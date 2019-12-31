6409cf0187a105c4b13986dd58cb79789784346b
augusin11 <augusin11@gmail.com>
2019-12-31 01:42:33 -0800

    V1.8 (complete)

--- app/src/main/java/com/example/undercover/RestartGame.java
+++ app/src/main/java/com/example/undercover/RestartGame.java
@@ -2,14 +2,25 @@ package com.example.undercover;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RestartGame extends AppCompatActivity {
+
+    private Button restartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restart_game);
+
+        restartButton = findViewById(R.id.buttonRestart);
     }
 
+    public void restartGame(View view) {
+        Intent intent = new Intent(this, GameSettings.class);
+        startActivity(intent);
+    }
 }
